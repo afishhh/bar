d9564e5621c752027fe68eda9148e1a39b61766e
Fishhh <hubert.gluchowski19@gmail.com>
2023-08-27 13:00:55 +0200

    Rework NetworkBlock with terrible code

--- src/blocks/network.cc
+++ src/blocks/network.cc
@@ -1,21 +1,195 @@
 #include <algorithm>
+#include <charconv>
+#include <concepts>
 #include <cstddef>
+#include <cstdint>
+#include <cstdlib>
 #include <filesystem>
 #include <fstream>
+#include <ios>
 #include <iostream>
+#include <limits>
+#include <mutex>
+#include <optional>
+#include <spawn.h>
+#include <stdexcept>
 #include <string>
+#include <string_view>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <system_error>
 #include <type_traits>
+#include <unistd.h>
+#include <unordered_map>
 #include <utility>
 
 #include "../format.hh"
 #include "../util.hh"
 #include "network.hh"
 
+auto const DEVICE_CLASS_ETHERNET = 0x020000;
+auto const DEVICE_CLASS_WIFI = 0x028000;
+
+void iwctl_update_station(WifiStation &station) {
+  pid_t pid;
+  posix_spawn_file_actions_t actions;
+  if (posix_spawn_file_actions_init(&actions) < 0)
+    throw std::system_error(errno, std::system_category(),
+                            "posix_spawn_file_actions_init");
+
+  int memfd = memfd_create("iwctl_output", 0);
+  if (memfd < 0)
+    throw std::system_error(errno, std::system_category(), "memfd_create");
+
+  if (posix_spawn_file_actions_adddup2(&actions, memfd, STDOUT_FILENO) < 0)
+    throw std::system_error(errno, std::system_category(),
+                            "posix_spawn_file_actions_adddup2");
+
+  posix_spawnattr_t attr;
+  if (posix_spawnattr_init(&attr) < 0)
+    throw std::system_error(errno, std::system_category(),
+                            "posix_spawnattr_init");
+
+  {
+    std::string sargv[] = {"iwctl", "station", "show"};
+    char *const argv[] = {sargv[0].data(), sargv[1].data(), station.name.data(),
+                          sargv[2].data(), nullptr};
+
+    if (posix_spawnp(&pid, "iwctl", &actions, &attr, argv, environ) < 0)
+      throw std::system_error(errno, std::system_category(), "posix_spawnp");
+  }
+
+  if (posix_spawn_file_actions_destroy(&actions) < 0)
+    throw std::system_error(errno, std::system_category(),
+                            "posix_spawn_file_actions_destroy");
+  if (posix_spawnattr_destroy(&attr) < 0)
+    throw std::system_error(errno, std::system_category(),
+                            "posix_spawnattr_destroy");
+
+  int wstatus;
+  while (waitpid(pid, &wstatus, 0) < 0 && errno == EINTR)
+    ;
+
+  std::ifstream out("/proc/self/fd/" + std::to_string(memfd), std::ios::binary);
+  if (!out)
+    throw std::runtime_error("Failed to open memfd with script output");
+
+  if (close(memfd) < 0)
+    throw std::system_error(errno, std::system_category(), "close");
+
+  if (!WIFEXITED(wstatus) || WEXITSTATUS(wstatus) != 0)
+    throw std::runtime_error(
+        std::format("iwctl station {} show exited with non-zero exit status {}",
+                    station.name, WEXITSTATUS(wstatus)));
+
+  std::unordered_map<std::string, std::string> properties;
+
+  for (uint64_t i = 0; i < 4; ++i)
+    out.ignore(std::numeric_limits<std::streamsize>().max(), out.widen('\n'));
+  out.ignore(std::numeric_limits<std::streamsize>().max(), out.widen(' '));
+
+  std::string k, v;
+  while (out >> k) {
+    while (true) {
+      // this will be a space character
+      out.get();
+      // if this is not a space character then the previous space was just a
+      // space in the property name
+      if (!out)
+        throw std::runtime_error("could not parse iwctl output");
+      else if (out.peek() != ' ') {
+        k += ' ';
+        // FIXME: don't do this
+        std::string next;
+        out >> next;
+        k += next;
+      } else
+        break;
+    }
+
+    std::getline(out, v);
+    v = trim(v);
+
+    properties.emplace(std::move(k), std::move(v));
+  }
+
+  IwctlStationInfo new_info;
+  new_info.scanning = properties["Scanning"] == "yes";
+  if (properties["State"] == "connected") {
+    auto _pop_prop = [&properties]<typename T>(std::string const &prop) {
+      auto it = properties.find(prop);
+      if (it == properties.end()) {
+        warn << "iwctl network missing property " << prop << '\n';
+        return T();
+      }
+      auto value = std::move(it->second);
+      properties.erase(it);
+
+      if constexpr (std::same_as<std::string, T>) {
+        return value;
+      } else {
+        T out{0};
+        auto result =
+            std::from_chars(value.c_str(), value.c_str() + value.size(), out);
+        if (result.ec != std::errc())
+          warn << "from_chars failed on iwctl property " << prop << " ("
+               << properties[prop] << ")" << '\n';
+        return out;
+      }
+    };
+
+#define pop_prop(tp, prop) _pop_prop.template operator()<tp>(prop)
+
+    new_info.connection = IwctlConnectionInfo{
+        .connected_network = pop_prop(std::string, "Connected network"),
+        .ipv4_address =
+            properties.contains("IPv4 address")
+                ? std::optional(pop_prop(std::string, "IPv4 address"))
+                : std::nullopt,
+        .connected_bss = pop_prop(std::string, "ConnectedBss"),
+        .security = pop_prop(std::string, "Security"),
+        .frequency = pop_prop(unsigned, "Frequency"),
+
+        .rssi = pop_prop(signed, "RSSI"),
+        .average_rssi = pop_prop(signed, "AverageRSSI"),
+
+        .others = std::move(properties)};
+
+#undef pop_prop
+  }
+
+  {
+    std::unique_lock lock(station.modify_mutex);
+    station.info = std::move(new_info);
+  }
+
+  station.update_running.store(false);
+}
+
+NetworkBlock::Config NetworkBlock::Config::autodetect() {
+  Config out;
+
+  for (auto entry : std::filesystem::directory_iterator("/sys/class/net/")) {
+    if (!std::filesystem::exists(entry.path() / "device"))
+      continue;
+
+    uint64_t device_class;
+    std::ifstream(entry.path() / "device" / "class") >> std::hex >>
+        device_class;
+
+    if (device_class == DEVICE_CLASS_ETHERNET)
+      out._ethernet_devices.push_back(entry.path().filename().string());
+    else if (device_class == DEVICE_CLASS_WIFI)
+      out._wifi_devices.push_back(entry.path().filename().string());
+  }
+
+  return out;
+}
+
 void NetworkBlock::update() {
   _tx_bytes = 0, _rx_bytes = 0;
   for (auto &n : std::filesystem::directory_iterator("/sys/class/net/")) {
-    if (!n.path().filename().string().starts_with("e") &&
-        !n.path().filename().string().starts_with("w"))
+    if (!std::filesystem::exists(n.path() / "device"))
       continue;
     auto tx = n.path() / "statistics" / "tx_bytes";
     auto rx = n.path() / "statistics" / "rx_bytes";
@@ -39,10 +213,101 @@ void NetworkBlock::update() {
 
   _last_tx_bytes = old_tx;
   _last_rx_bytes = old_rx;
+
+  for (auto const &name : _config._ethernet_devices) {
+    std::ifstream(std::filesystem::path("/sys/class/net/") / name /
+                  "carrier") >>
+        _ethernet_connected;
+    if (_ethernet_connected)
+      break;
+  }
+
+  for (auto &station : _wifi_stations) {
+    bool ex = false;
+    if (station.update_running.compare_exchange_strong(ex, true))
+      iwctl_update_station(station);
+  }
 }
 
 size_t NetworkBlock::draw(ui::draw &draw, std::chrono::duration<double>) {
-  return draw.text(0, std::format("Tx {: >8}/s Rx {: >8}/s",
-                                  to_sensible_unit(_tx_bytes, 1),
-                                  to_sensible_unit(_rx_bytes, 1)));
+  auto x = 0;
+
+  if (_ethernet_connected)
+    x += draw.text(x, " ");
+
+  for (auto const &station : _wifi_stations) {
+    std::unique_lock lock(station.modify_mutex);
+    if (station.info.connection)
+      x += draw.text(x, " ");
+    if (station.info.scanning) {
+      x += draw.text(x, "󰍉 ");
+      if (!station.info.connection)
+        x += draw.text(x, station.name);
+    }
+    if (station.info.connection)
+      x += draw.text(x, station.info.connection->connected_network);
+  }
+
+  if (x == 0)
+    x += draw.text(x, "Not connected");
+
+  return x;
+}
+
+void NetworkBlock::draw_tooltip(ui::draw &draw, std::chrono::duration<double>,
+                                unsigned int) const {
+  auto width = 250;
+  draw.line(0, 0, width, 0, 0);
+
+  auto y = 0;
+  for (auto const &station : _wifi_stations) {
+    std::unique_lock lock(station.modify_mutex);
+
+    if (!station.info.scanning && !station.info.connection)
+      continue;
+
+    auto const &t = station.name;
+    draw.text((width - draw.textw(t)) / 2, 12 + y, t);
+
+    y += 20;
+
+    if (station.info.scanning) {
+      auto t = "Scanning...";
+      draw.text((width - draw.textw(t)) / 2, 12 + y, t, 0x8888FF);
+      y += 20;
+    }
+
+    if (station.info.connection) {
+      auto const &conn = station.info.connection;
+
+      unsigned x = 0;
+      x += draw.text(x, 12 + y, "Connected to ");
+      draw.text(x, 12 + y, conn->connected_network, 0xFFAA00);
+
+      y += 20;
+
+      {
+        draw.text(0, 12 + y, std::format("RSSI: {}", conn->rssi));
+        auto t = std::format("Security: {}", conn->security);
+        draw.text(width - draw.textw(t), 12 + y, t);
+      }
+
+      y += 20;
+
+      std::string_view ip = "Unknown";
+      if (conn->ipv4_address)
+        ip = conn->ipv4_address.value();
+      draw.text(0, 12 + y, std::format("IP: {}", ip));
+
+      y += 20;
+    }
+  }
+
+  if (y)
+    y += 10;
+
+  draw.text(0, 12 + y,
+            std::format("Rx {: >8}/s", to_sensible_unit(_rx_bytes, 1)));
+  auto t = std::format("Tx {: >8}/s", to_sensible_unit(_tx_bytes, 1));
+  draw.text((width - draw.textw(t)), 12 + y, t);
 }
--- src/blocks/network.hh
+++ src/blocks/network.hh
@@ -1,19 +1,76 @@
 #pragma once
 
+#include <atomic>
 #include <chrono>
 #include <cstddef>
+#include <forward_list>
+#include <mutex>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 #include "../block.hh"
 
+struct IwctlConnectionInfo {
+  std::string connected_network;
+  std::optional<std::string> ipv4_address;
+  std::string connected_bss;
+  std::string security;
+  unsigned frequency;
+
+  signed rssi;
+  signed average_rssi;
+
+  std::unordered_map<std::string, std::string> others;
+};
+
+struct IwctlStationInfo {
+  bool scanning;
+  std::optional<IwctlConnectionInfo> connection;
+};
+
+struct WifiStation {
+  std::string name;
+  mutable std::mutex modify_mutex;
+  std::atomic_bool update_running;
+  IwctlStationInfo info;
+
+  WifiStation(std::string const &name) : name(name) {}
+};
+
+// iwctl only for now
 class NetworkBlock : public Block {
   size_t _last_tx_bytes = 0, _last_rx_bytes = 0;
   size_t _tx_bytes, _rx_bytes;
 
+  bool _ethernet_connected;
+  std::forward_list<WifiStation> _wifi_stations;
+
+  struct Config {
+    std::vector<std::string> _ethernet_devices;
+    std::vector<std::string> _wifi_devices;
+
+    static Config autodetect();
+  };
+
+private:
+  Config _config;
+
 public:
+  NetworkBlock() : NetworkBlock(Config::autodetect()) {}
+  NetworkBlock(Config config) : _config(std::move(config)) {
+    for (auto const &name : _config._wifi_devices)
+      _wifi_stations.emplace_front(name);
+  }
+  ~NetworkBlock() {}
+
   size_t draw(ui::draw &, std::chrono::duration<double> delta) override;
   void update() override;
   std::chrono::duration<double> update_interval() override {
-    // NOTE: The unit in draw() should albo be changed if this ever changes.
     return std::chrono::milliseconds(500);
   }
+
+  bool has_tooltip() const override { return true; }
+  void draw_tooltip(ui::draw &, std::chrono::duration<double>,
+                    unsigned int) const override;
 };
